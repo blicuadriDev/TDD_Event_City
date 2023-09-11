38e0182892dc2ee55af8ee78168fdbfa0ebe63c1
blicuadriDev <blicuadriDev@gmail.com>
2023-09-11 09:08:20 -0300

    TDD Event_city project started

---
+++ src/main/java/com/devsuperior/tdd_event_city/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.tdd_event_city;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
